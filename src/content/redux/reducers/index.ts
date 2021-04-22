32cc85a3da0054c47a7676a0275be2f176097bcf
Fleris <fleris.ramirez@socobox.co>
2021-04-22 10:09:18 -0500

    Combinar reducers.

--- src/content/redux/reducers/index.ts
+++ src/content/redux/reducers/index.ts
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
 import {movieReducer} from './MovieReducer';
-
+import {movieFavorites} from './MovieFavoriteReducer'
 
 
 export default combineReducers({
   movieReducer,
-  
+  movieFavorites,
 })

d20ec277f962ba5baf18cc10028f339f938825bb
Fleris <fleris.ramirez@socobox.co>
2021-04-22 09:57:00 -0500

    Configuración redux-typescript.

---
+++ src/content/redux/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import {movieReducer} from './MovieReducer';
+
+
+
+export default combineReducers({
+  movieReducer,
+  
+})
---
+++ src/store.ts
@@ -0,0 +1,33 @@
+import { createStore, applyMiddleware, } from 'redux';
+
+import rootReducer from './content/redux/reducers/index';
+import {save, load} from 'redux-localstorage-simple';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import createSagaMiddleware from 'redux-saga';
+
+
+const sagaMiddleware = createSagaMiddleware();
+const initialData= save(
+    {
+      states:["movieReducer"
+     ]
+    }    
+  );
+  
+  
+  const createStoreWithMiddleware 
+  = 
+  composeWithDevTools(applyMiddleware(sagaMiddleware,initialData),)
+      
+  (createStore)
+  
+  
+    const store = createStoreWithMiddleware(
+      rootReducer,    
+       load({
+            states:["movieReducer"
+           ]     
+       }) 
+   )  
+  //  sagaMiddleware.run(rootSaga)
+  export default store;

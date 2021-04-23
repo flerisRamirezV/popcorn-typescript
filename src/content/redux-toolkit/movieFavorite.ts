575d3194131defc6b82d16d2b3efe39609413beb
Fleris <fleris.ramirez@socobox.co>
2021-04-23 15:06:43 -0500

    Agregada funcionalidad para mostrar el modal.

--- src/content/redux-toolkit/movieFavorite.ts
+++ src/content/redux-toolkit/movieFavorite.ts
@@ -1,26 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Imovie } from '../redux/interfaces/interfaces';
+import { Imovie } from "../redux/interfaces/interfaces";
 
 const initialState: Imovie = {
-  favorite:[],
-  noFavorite:[],
+  favorite: [],
+  noFavorite: [],
   saveFavorite: null,
-  modal:false,
-  inputValue:"",
-}
+  modal: false,
+  inputValue: "",
+};
 
 export const movieFavoriteSlice = createSlice({
   name: "movie",
   initialState,
   reducers: {
     getSaveDataInput: (state, action) => {
-     
-      state.inputValue= action.payload
-   }
-  }
+      state.inputValue = action.payload;
+    },
+    saveMovieFavorite: (state, action) => {
+      state.saveFavorite=action.payload
+    },
+    toogleModal: (state) => {
+      state.modal=!state.modal
+    }
+  },
 });
 
 // Action creators are generated for each case reducer function
- export const { getSaveDataInput } = movieFavoriteSlice.actions;
+export const { getSaveDataInput,saveMovieFavorite,toogleModal } = movieFavoriteSlice.actions;
 
 export default movieFavoriteSlice.reducer;

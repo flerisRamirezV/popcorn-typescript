b3a78f61a26c0410e860cd8c9295cf7f7db43b11
Fleris <fleris.ramirez@socobox.co>
2021-04-23 15:55:05 -0500

    Agregado componente para mostrar las películas favoritas.

--- src/content/components/MyFavoriteMovie.tsx
+++ src/content/components/MyFavoriteMovie.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import { useSelector, useDispatch} from "react-redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { typeMovieReducer } from "../redux/interfaces/interfaces";
+import "../styles/favorite.css";
+import Favorite from "./favorite";
+
 export default function MyFavoriteMovie() {
+  const favorite = useSelector(
+    (state: typeMovieReducer) => state.movieFavorites.favorite
+  );
+  const dispatch = useDispatch();
   return (
-    <div>
-      
+    <div className="favorite">
+      <h2 className="favorite__title">My favorites</h2>
+      <hr />
+      {favorite.length === 0 && (
+        <p className="favorite__p">You haven´t favorired any movies yet</p>
+      )}
+      <section className="container__movie">
+        {favorite.map((fav, index) => (
+          <Favorite
+            key={fav.id}
+            {...fav}
+            // handleDelete={() => handleDelete(fav.id)}
+          />
+        ))}
+      </section>
     </div>
-  )
+  );
 }

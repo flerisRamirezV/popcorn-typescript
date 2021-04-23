009b69aa327ee524c8b8a49af8b7b8587e251eba
Fleris <fleris.ramirez@socobox.co>
2021-04-23 14:19:36 -0500

    Agregado selectores.

--- src/content/views/PopCorn.tsx
+++ src/content/views/PopCorn.tsx
@@ -1,26 +1,27 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import { getMovie } from "../redux/actions/getMovies";
 import InputMovie from "../components/InputMovie";
 import Movies from "../components/movies";
 import { toogleModal } from "../helpers/events";
 import { movieFavorite } from "../redux/actions/favorite";
 import TitleMovies from "../components/TitleMovies";
-import { typeMovieReducer } from '../redux/interfaces/interfaces';
-import { getMovie } from '../redux-toolkit/movieReducer';
+import { typeMovieReducer } from "../redux/interfaces/interfaces";
+import * as constants from "../redux/constants/constants"
 export default function PopCorn() {
   const dispatch = useDispatch();
   // const modal = useSelector((state: typeMovieReducer) => state.movieFavorites.modal);
-
+  const movieRated = useSelector(
+    (state: typeMovieReducer) => state.movieReducer.moviesRated
+  );
 
   useEffect(() => {
-     dispatch({type:"GET_MOVIES"});
+    dispatch({ type:constants.GET_MOVIES});
+    dispatch({ type: constants.GET_MOVIES_RATED });
   }, [dispatch]);
 
-
-   const movieList = useSelector((state: typeMovieReducer) => {
+  const movieList = useSelector((state: typeMovieReducer) => {
     return state.movieReducer.filterMovie;
-   });
+  });
 
   const handleDetail = (mov: any) => {
     // dispatch(toogleModal(!modal:boolean));
@@ -30,10 +31,10 @@ export default function PopCorn() {
   return (
     <main className="container">
       <section className="container__input-movie">
-        {/* <InputMovie /> */}
+         <InputMovie /> 
       </section>
-      <TitleMovies title="Popular Movies"/>
-       <section className="container__overflow">
+      <TitleMovies title="Popular Movies" />
+      <section className="container__overflow">
         {movieList.length > 0 &&
           movieList.map((movie: any): any => (
             <Movies
@@ -42,7 +43,18 @@ export default function PopCorn() {
               handleDetail={() => handleDetail(movie)}
             />
           ))}
-      </section> 
+      </section>
+      <TitleMovies title="Top Rated" />
+      <section className="container__overflow">
+        {movieRated.length > 0 &&
+          movieRated.map((movie: any): any => (
+            <Movies
+              key={movie.id}
+              {...movie}
+              handleDetail={() => handleDetail(movie)}
+            />
+          ))}
+      </section>
     </main>
   );
 }

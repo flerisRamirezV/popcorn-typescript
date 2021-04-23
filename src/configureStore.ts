76c699f23dc9ac96f9286b0c545b6dab56b707f7
Fleris <fleris.ramirez@socobox.co>
2021-04-23 14:14:52 -0500

    Agregado reducer.

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import rootSaga from "./content/redux/redux-saga/rootSaga";
 import createSagaMiddleware from "redux-saga";
 import movieSlice from "./content/redux-toolkit/movieReducer";
+import movieFavoriteSlice from './content/redux-toolkit/movieFavorite';
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 export const store = configureStore({
   reducer: {
     movieReducer: movieSlice,
+    movieFavorites:movieFavoriteSlice,
   },
   middleware,
 });

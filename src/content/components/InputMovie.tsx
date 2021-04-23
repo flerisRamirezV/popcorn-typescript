5ec2ec608a1402515f0fe67db6a91e700f3834d6
Fleris <fleris.ramirez@socobox.co>
2021-04-23 14:15:15 -0500

    Agregada funcionalidad para filtrar data.

--- src/content/components/InputMovie.tsx
+++ src/content/components/InputMovie.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "../styles/movie.css";
 import { useDispatch,useSelector } from "react-redux";
-import { saveDataInput, filterMovieData } from '../redux/actions/filterData';
 import { typeMovieReducer,FormElemnt } from '../redux/interfaces/interfaces';
-
+import * as constant from '../redux/constants/constants';
+import { getSaveDataInput } from '../redux-toolkit/movieFavorite';
 
 export default function InputMovie() {
   const inputValue = useSelector((state:typeMovieReducer) => state.movieFavorites.inputValue)
   const dispatch = useDispatch();
 
   const filterByName = (e:FormElemnt):any => {
-    dispatch(saveDataInput(e.target.value))
+     dispatch(getSaveDataInput(e.target.value))
    
     setTimeout(()=>{
       
-      dispatch(filterMovieData(e.target.value));
+     dispatch({ type:constant.FILTER_MOVIE, data:e.target.value});
     },1000)
      
   };
@@ -22,8 +22,8 @@ export default function InputMovie() {
     <div className="container__input">
       <input
         className="form__input"
-         value={inputValue}
-         onChange={filterByName}
+          value={inputValue}
+          onChange={filterByName}
       />
       <i className="fas fa-search form__icon"></i>
     </div>

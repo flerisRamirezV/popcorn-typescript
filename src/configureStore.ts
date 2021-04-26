6d9606d4a1e786d3f4214eefe7162ccecc84dea7
Fleris <fleris.ramirez@socobox.co>
2021-04-26 07:51:28 -0500

    Agregada librería lodash.

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,18 +2,41 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import rootSaga from "./content/redux/redux-saga/rootSaga";
 import createSagaMiddleware from "redux-saga";
 import movieSlice from "./content/redux-toolkit/movieReducer";
-import movieFavoriteSlice from './content/redux-toolkit/movieFavorite';
+import movieFavoriteSlice from "./content/redux-toolkit/movieFavorite";
+
+import {
+  loadState,
+  saveState,
+} from "./content/redux-toolkit/actions/localStoraje";
+import throttle from "lodash/throttle";
 const sagaMiddleware = createSagaMiddleware();
 
-const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
+ const persistedState = loadState();
+
+
+
+const middleware = [
+  ...getDefaultMiddleware({
+    thunk: false,
+    serializableCheck: false,
+    persistedState,
+  }),
+  sagaMiddleware,
+];
+
 export const store = configureStore({
   reducer: {
     movieReducer: movieSlice,
-    movieFavorites:movieFavoriteSlice,
+    movieFavorites: movieFavoriteSlice,
   },
   middleware,
 });
 
+
+
+store.subscribe(
+  throttle(() => saveState(store.getState().movieFavorites.favorite ), 1000)
+);
 
 export type RootState = ReturnType<typeof store.getState>;
 sagaMiddleware.run(rootSaga);
